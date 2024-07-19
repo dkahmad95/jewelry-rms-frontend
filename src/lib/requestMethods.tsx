b45153b3a346628d6c64485599c3aae6c11781ca
dkahmad95 <dkahmad95@gmail.com>
2024-07-19 17:38:20 +0300

    pre-deplyment4

--- src/lib/requestMethods.tsx
+++ src/lib/requestMethods.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import {fetchAuthSession} from "@aws-amplify/auth";
 
-const BASE_URL = "http://localhost:8080/api/";
+const BASE_URL = "http://127.0.0.1:8080";
 
 export const TOKEN = async () => {
 
